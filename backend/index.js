0cc3878807f26aaf652b2d95912b6d5b62f641e4
imdadhossain1 <imdadhossain24@gmail.com>
2023-01-07 21:14:18 +0800

    Base backend

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
 require("dotenv").config() // Load environment variables from .env | PG_HOST = host:port | PG_USER | PG_PW(password)
+
+const express = require('express')
+const http = require('http');
+const { initDB } = require('./database');
+
+const app = express()
+
+http.createServer(app).listen(3001);
+initDB()
