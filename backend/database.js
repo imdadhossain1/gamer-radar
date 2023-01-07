3189cb91e85b7157e60860d1607fe5504a8da4e8
imdadhossain1 <imdadhossain24@gmail.com>
2023-01-08 03:46:35 +0800

    Add Genre Sequelize Model

--- backend/database.js
+++ backend/database.js
@@ -4,10 +4,22 @@ const { PG_HOST, PG_USER, PG_PW } = process.env
 
 const sequelize = new Sequelize(`postgres://${PG_USER}:${PG_PW}@${PG_HOST}/gamer-radar`)
 
+const Genres = sequelize.define('Genres', {
+  id: {
+    type: DataTypes.BIGINT,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: DataTypes.STRING,
+  allowNull: false
+})
+
 module.exports.initDB = async () => {
   try {
     await sequelize.authenticate()
     console.log('Connected to DB')
+    
   } catch (e) {
     console.error('Unable to Connect to DB', e)
   }
