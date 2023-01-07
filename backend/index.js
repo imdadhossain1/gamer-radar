cbc866f50e54eee659aa083ca4d7b36ff9d8dbea
imdadhossain1 <imdadhossain24@gmail.com>
2023-01-08 02:26:34 +0800

    Basic React Routing

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,14 @@ const express = require('express')
 const http = require('http');
 const { initDB } = require('./database');
 
+const authRouter = require('./routes/auth.js')
+
 const app = express()
 const server = http.createServer(app)
 
 const sio = require('socket.io')(server)
+
+app.use('/auth', authRouter)
 
 server.listen(3001)
 initDB()
